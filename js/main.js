123cc08e11a91a98b5c452a41a917def5b7bcad5
Igor Stakic <stakic@mdpi.com>
2018-07-20 15:31:17 +0200

    add video

--- js/main.js
+++ js/main.js
@@ -1,9 +1,28 @@
 $(document).ready(function() {
-    $(window).scrollTop() > 300 ? $('.header').addClass('header--fixed') : 
-    $('.header').removeClass('header--fixed');
-    $(window).on('scroll', function() {
-        $(window).scrollTop() > 300 ? $('.header').addClass('header--fixed') : 
+
+    $('.teaser').on('click', function() {
+        $(".teaser__video")[0].play()
+    });
+
+
+    if($(window).scrollTop() > 300) {
+        $('.header').addClass('header--fixed');
+        $('.header__logo__img').css({'display':'block'});
+    } else {
         $('.header').removeClass('header--fixed');
+        $('.header__logo__img').css({'display':'none'});
+    }
+    $(window).on('scroll', function() {
+        // $(window).scrollTop() > 300 ? $('.header').addClass('header--fixed') $('.header__logo__img').css({'display':'none'}) : 
+        // $('.header').removeClass('header--fixed') $('.header__logo__img').css({'display':'none'});
+
+        if($(window).scrollTop() > 300) {
+            $('.header').addClass('header--fixed');
+            $('.header__logo__img').css({'display':'block'});
+        } else {
+            $('.header').removeClass('header--fixed');
+            $('.header__logo__img').css({'display':'none'});
+        }
     })
 
 })
