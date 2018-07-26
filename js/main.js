010f0772117183e2712265a1992a2103fe1d2045
stakic-igor <stakic_igor@yahoo.com>
2018-07-26 08:08:13 +0200

    add responsive menu

--- js/main.js
+++ js/main.js
@@ -1,9 +1,18 @@
 $(document).ready(function() {
-
-    $('.icon-play').on('click', function() {
-        $(".teaser__video")[0].play()
+        $('.icon-play').on('click', function() {
+        $(".teaser__video")[0].play();
+        $('.icon-pause').removeClass('icon--hide');
+        $('.icon-pause').addClass('icon--show');
+        $('.icon-play').removeClass('icon--show');
+        $('.icon-play').addClass('icon--hide');
+        
     });
-
+    $('.icon-pause').on('click', function() {
+        $('.teaser__video')[0].pause();
+        $('.icon-play').addClass('icon--show');
+        $('.icon-pause').removeClass('icon--show');
+        $('.icon-pause').addClass('icon--hide');
+    })
 
     if($(window).scrollTop() > 300) {
         $('.header').addClass('header--fixed');
